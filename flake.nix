f4b55fe01e2cdf54fb3d89681e10fc442bd855e6
Thiago-LPB <105386839+Thiago-LPB@users.noreply.github.com>
2026-06-23 03:43:55 -0300

    first-commit

---
+++ flake.nix
@@ -0,0 +1,126 @@
+{
+  description = "Coppelia-nix flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    
+    pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+      pyzmq 
+      cbor2     
+      numpy
+      ipykernel
+    ]);
+
+    rpmSrc = pkgs.fetchurl {
+        url = "https://rpmfind.net/linux/mageia/distrib/9/x86_64/media/core/updates/lib64sodium23-1.0.18-3.1.mga9.x86_64.rpm";
+        hash = "sha256-C7fmrGQqEV4xDalP8MbW84FNJg+jMbXY1QILkHFQ2xs=";
+    };
+
+    buildInputs = let
+
+          xorg-deps = with pkgs; [
+            libX11
+            libXau
+            libXcursor
+            libXdmcp
+            libXrender
+            libxcb
+          ];
+
+         qt-deps = with pkgs.libsForQt5; [
+          qtbase
+          qtsvg
+         ];
+
+        in with pkgs; [
+          dbus
+          ffmpeg_4.lib
+          fontconfig
+          freetype
+          glib
+          libGL
+          libkrb5
+          libxkbcommon
+          stdenv.cc.cc
+          zlib
+          libbsd
+        ] ++ xorg-deps ++ qt-deps;
+
+    ld_path = pkgs.lib.makeLibraryPath buildInputs;
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      inherit buildInputs;
+      pname = "Coppelia-nix";
+      version = "0.1.0";
+
+      # src = pkgs.lib.cleanSourceWith {
+      #   src = ./.;
+      #   filter = path: type:
+      #     !(pkgs.lib.hasInfix "/build/" path)
+      #     && (baseNameOf path != "flake.nix")
+      #     && (baseNameOf path != "flake.lock")
+      #     && (baseNameOf path != "result");
+      # };
+      src = pkgs.fetchurl {
+        url = "https://downloads.coppeliarobotics.com/V4_10_0_rev0/CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu24_04.tar.xz";
+        hash = "sha256-+2KUfDynAV5/UmgwrqwEoeeRQCc2itIjCmQZD5f2i7o=";
+      };
+
+      nativeBuildInputs = with pkgs; [ 
+        makeWrapper 
+        libarchive
+      ];
+
+      # unpackPhase = ''
+      #   runHook preUnpack
+      #
+      #   dirName=$(tar -tf "$src" | head -1 | cut -d/ -f1)
+      #   tar -xf $src
+      #   mv $dirName coppeliaSrc
+      #
+      #   bsdtar -xf ${rpmSrc}       
+      #
+      #   runHook postUnpack
+      # '';
+
+      postUnpack = ''
+        bsdtar -xf ${rpmSrc}
+        cp -r usr/lib64/* $sourceRoot
+      '';
+
+      installPhase = ''
+        set -x
+
+        mkdir -p $out/bin
+
+        cp -r ./* $out/
+
+        makeWrapper $out/coppeliaSim.sh $out/bin/coppeliaSim \
+        --set LD_LIBRARY_PATH "${ld_path}" \
+        --prefix PATH : "${pythonEnv}/bin"
+
+      '';
+      dontWrapQtApps = true;
+    
+
+      meta = {
+        description = "Copelia-nix";
+        maintainers = [ "Tlpb" ];
+      };
+    };
+    apps.${system}.default = {
+      type = "app";
+      program = "${self.packages.${system}.default}/bin/coppeliaSim";
+    };
+  };
+}
